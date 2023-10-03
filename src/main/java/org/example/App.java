0451b4e1ce707e805a03c940ac1f017604c50808
JRVelarde <jonrvelarde@gmail.com>
2023-10-03 11:59:46 +0200

    Clases Contacto y ContactoAgenda

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,44 @@
 package org.example;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ContactoAgenda agenda = new ContactoAgenda();
+        Scanner sc = new Scanner(System.in);
+
+    public String Menu(){
+
+        int opcion = 0;
+        while(true){
+            System.out.println("1.Buscar por código " +
+                    "2.Buscar por inicio de nombre " +
+                    "3.Mostrar Agenda " +
+                    "4.Añadir contacto " +
+                    "5.Borrar contacto " +
+                    "0.Salir");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion){
+                case 1:
+                    System.out.println("Escribe el código del usuario");
+                    UUID codigo = UUID.fromString(sc.nextLine());
+                    List<Contacto> buscarContacto =
+                    break;
+
+            }
+        }
+    }
+
+
     }
 }
